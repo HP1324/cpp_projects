2d15bb99ac44ed259c34edde6a43d0eb694a2987
husen <iamhusen101@gmail.com>
2024-07-03 16:24:28 +0530

    Initial add, view and search added

--- contact_management/contact_manager.cpp
+++ contact_management/contact_manager.cpp
@@ -1,20 +1,125 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-class Contact{
-    private:
-        string firstName;
-        string lastName;
-        string phoneNumber;
-        string emailAddress;
-        string address;
+class Contact;
+Contact addContact();
+int searchContact(string searchKey, const vector<Contact> &contacts);
+void viewContacts(const vector<Contact> &contacts);
+class Contact
+{
+private:
+    string firstName;
+    string lastName;
+    string phoneNumber;
+    string emailAddress;
 
+public:
+    friend Contact addContact();
+    friend void viewContacts(const vector<Contact> &contacts);
+    friend int searchContact(string searchKey, const vector<Contact> &contacts);
+    void print(){
+        cout << "First Name   : " << firstName<< '\n';
+        cout << "Last Name    : " << lastName <<'\n';
+        cout << "Phone Number : " << phoneNumber<< '\n';
+        cout << "Email Address: " << emailAddress<<'\n';
+    }
 };
+
 void showMenu();
-void addContact();
-void viewContacts();
+
 void deleteContact(int index);
-int searchContact(Contact contacts, std::string key);
-int main(){
-    cout << "Welcome to Contact Manager!\nWhat do you want to do?: ";
-    return 0;
+int main()
+{
+    vector<Contact> contacts;
+
+    showMenu();
+    int input;
+    do
+    {
+        cin >> input;
+        switch (input)
+        {
+        case 1:
+            contacts.push_back(addContact());
+            showMenu();
+            break;
+        case 2:
+            viewContacts(contacts);
+            showMenu();
+            break;
+        case 3:
+            string searchKey;
+            cout << "Enter one of the following to search \n (First Name, Last Name, Phone Number, Email Address): ";
+            cin.ignore();
+            getline(cin, searchKey);
+            int result = searchContact(searchKey,contacts);
+            if(result == -1) cout << "Contact not found!";
+            else {
+                cout << "Contact found at index " << result << '\n';
+                contacts[result-1].print();
+                }
+            showMenu();
+            break;
+        }
+
+    } while (input != 6);
+}
+void showMenu()
+{
+    cout << "Contact book menu:\n\n";
+    cout << "1. Add Contacts\n";
+    cout << "2. View Contacts\n";
+    cout << "3. Search Contacts\n";
+    cout << "4. Edit Contact\n";
+    cout << "5. Delete Contact\n";
+    cout << "6. Exit\n\n";
+
+    cout << "Please choose an option(1-6): ";
+}
+Contact addContact()
+{
+    Contact contact;
+    cin.ignore();
+    cout << "First name: ";
+    getline(cin, contact.firstName);
+    cout << "Last name:  ";
+    getline(cin, contact.lastName);
+    cout << "Phone Number: ";
+    getline(cin, contact.phoneNumber);
+    cout << "Email : ";
+    getline(cin, contact.emailAddress);
+
+    cout << "Contact added successfully\n";
+    return contact;
+}
+void viewContacts(const vector<Contact> &contacts)
+
+{
+    if (contacts.empty())
+        cout << "Contact list empty! add some contacts first\n";
+    else
+    {
+        for (int i = 0; i < (int)contacts.size(); ++i)
+        {
+            cout << "\n*****Contact List*****\n\n";
+            cout << "index\tFirst Name\tSecond Name\tPhone Number\tEmail Address\n";
+            cout << i + 1 << "\t  ";
+            cout << contacts[i].firstName << "\t\t";
+            cout << contacts[i].lastName << "\t\t ";
+            cout << contacts[i].phoneNumber << "\t";
+            cout << contacts[i].emailAddress << '\n';
+        }
+    }
+}
+
+int searchContact(string searchKey, const vector<Contact> &contacts)
+{
+    for (auto i = 0; i < contacts.size(); ++i)
+    {
+        if (searchKey == contacts[i].firstName || searchKey == contacts[i].lastName || searchKey == contacts[i].phoneNumber || searchKey == contacts[i].emailAddress){
+            return i + 1;
+        }else{
+            return -1;
+        }
+    }
 }
